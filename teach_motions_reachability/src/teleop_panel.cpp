67f0e3e2a49ab77c5c8c9ab8e3dfd8ebb4061fe7
AndyZe <andyz@utexas.edu>
2018-05-21 16:41:02 -0500

    Major re-org to add a reachability visualization.

---
+++ teach_motions_reachability/src/teleop_panel.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+
+#include <QPainter>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QTimer>
+
+#include <geometry_msgs/Twist.h>
+#include "teleop_panel.h"
+
+namespace teach_motions_reachability
+{
+
+// BEGIN_TUTORIAL
+// Here is the implementation of the TeleopPanel class.  TeleopPanel
+// has these responsibilities:
+//
+// - Act as a container for GUI elements QLineEdit.
+// - Saving and restoring internal state from a config file.
+//
+// We start with the constructor, doing the standard Qt thing of
+// passing the optional *parent* argument on to the superclass
+// constructor
+TeleopPanel::TeleopPanel( QWidget* parent )
+  : rviz::Panel( parent )
+{
+  // Next we lay out the "output topic" text entry field using a
+  // QLabel and a QLineEdit in a QHBoxLayout.
+  QHBoxLayout* topic_layout = new QHBoxLayout;
+  topic_layout->addWidget( new QLabel( "Output Topic:" ));
+  output_topic_editor_ = new QLineEdit;
+  topic_layout->addWidget( output_topic_editor_ );
+
+  // Lay out the topic field above the control widget.
+  QVBoxLayout* layout = new QVBoxLayout;
+  layout->addLayout( topic_layout );
+  setLayout( layout );
+
+  // Next we make signal/slot connections.
+  connect( output_topic_editor_, SIGNAL( editingFinished() ), this, SLOT( updateTopic() ));
+}
+
+// Read the topic name from the QLineEdit and call setTopic() with the
+// results.  This is connected to QLineEdit::editingFinished() which
+// fires when the user presses Enter or Tab or otherwise moves focus
+// away.
+void TeleopPanel::updateTopic()
+{
+  setTopic( output_topic_editor_->text() );
+}
+
+// Set the topic name we are publishing to.
+void TeleopPanel::setTopic( const QString& new_topic )
+{
+  // Only take action if the name has changed.
+  if( new_topic != output_topic_ )
+  {
+    output_topic_ = new_topic;
+    // If the topic is the empty string, don't publish anything.
+    if( output_topic_ == "" )
+    {
+      velocity_publisher_.shutdown();
+    }
+    else
+    {
+      // The old ``velocity_publisher_`` is destroyed by this assignment,
+      // and thus the old topic advertisement is removed.  The call to
+      // nh_advertise() says we want to publish data on the new topic
+      // name.
+      velocity_publisher_ = nh_.advertise<geometry_msgs::Twist>( output_topic_.toStdString(), 1 );
+    }
+    // rviz::Panel defines the configChanged() signal.  Emitting it
+    // tells RViz that something in this panel has changed that will
+    // affect a saved config file.  Ultimately this signal can cause
+    // QWidget::setWindowModified(true) to be called on the top-level
+    // rviz::VisualizationFrame, which causes a little asterisk ("*")
+    // to show in the window's title bar indicating unsaved changes.
+    Q_EMIT configChanged();
+  }
+}
+
+// Save all configuration data from this panel to the given
+// Config object.  It is important here that you call save()
+// on the parent class so the class id and panel name get saved.
+void TeleopPanel::save( rviz::Config config ) const
+{
+  rviz::Panel::save( config );
+  config.mapSetValue( "Topic", output_topic_ );
+}
+
+// Load all configuration data for this panel from the given Config object.
+void TeleopPanel::load( const rviz::Config& config )
+{
+  rviz::Panel::load( config );
+  QString topic;
+  if( config.mapGetString( "Topic", &topic ))
+  {
+    output_topic_editor_->setText( topic );
+    updateTopic();
+  }
+}
+
+} // end namespace teach_motions_reachability
+
+// Tell pluginlib about this class.  Every class which should be
+// loadable by pluginlib::ClassLoader must have these two lines
+// compiled in its .cpp file, outside of any namespace scope.
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(teach_motions_reachability::TeleopPanel,rviz::Panel )
+// END_TUTORIAL
---
+++ teach_motions_reachability/src/teleop_panel.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef TELEOP_PANEL_H
+#define TELEOP_PANEL_H
+
+#ifndef Q_MOC_RUN
+# include <ros/ros.h>
+
+# include <rviz/panel.h>
+#endif
+
+class QLineEdit;
+
+namespace teach_motions_reachability
+{
+
+class TeleopPanel: public rviz::Panel
+{
+// This class uses Qt slots and is a subclass of QObject, so it needs
+// the Q_OBJECT macro.
+Q_OBJECT
+public:
+  // QWidget subclass constructors usually take a parent widget
+  // parameter (which usually defaults to 0).  At the same time,
+  // pluginlib::ClassLoader creates instances by calling the default
+  // constructor (with no arguments).  Taking the parameter and giving
+  // a default of 0 lets the default constructor work and also lets
+  // someone using the class for something else to pass in a parent
+  // widget as they normally would with Qt.
+  TeleopPanel( QWidget* parent = 0 );
+
+  // Now we declare overrides of rviz::Panel functions for saving and
+  // loading data from the config file.  Here the data is the
+  // topic name.
+  virtual void load( const rviz::Config& config );
+  virtual void save( rviz::Config config ) const;
+
+  // Next come a couple of public Qt slots.
+public Q_SLOTS:
+  // In this example setTopic() does not get connected to any signal
+  // (it is called directly), but it is easy to define it as a public
+  // slot instead of a private function in case it would be useful to
+  // some other user.
+  void setTopic( const QString& topic );
+
+  // Here we declare some internal slots.
+protected Q_SLOTS:
+  // updateTopic() reads the topic name from the QLineEdit and calls
+  // setTopic() with the result.
+  void updateTopic();
+
+  // Then we finish up with protected member variables.
+protected:
+  // One-line text editor for entering the outgoing ROS topic name.
+  QLineEdit* output_topic_editor_;
+
+  // The current name of the output topic.
+  QString output_topic_;
+
+  // The ROS publisher for the command velocity.
+  ros::Publisher velocity_publisher_;
+
+  // The ROS node handle.
+  ros::NodeHandle nh_;
+};
+
+} // end namespace teach_motions_reachability
+
+#endif // TELEOP_PANEL_H
